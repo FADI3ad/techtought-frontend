b92b3e5f6e5f89bc6080708106f826590638fdab
mariam <mkopibhh03@gmail.com>
2026-01-22 18:50:38 +0200

    Add simple navbar and header section

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,24 @@
 
 <template>
 
+<section class="container">
+  <h1 class="h1">Skills to transform your career and life </h1>
+  <p class="p1">From critical skills to technical topics, Techtought supports your professional development.</p>
+</section>
+<!-- Navbar -->
+  <nav class="nav">
+    <a href="#">Computer Fundamentals</a> 
+    <a href="#">Web Development</a> 
+    <a href="#">Mobile Development</a>
+    <a href="#">Artificial Intelligence (AI)</a>
+  </nav>
 </template>
 
 <style scoped>
-
 </style>
+
+
+
+
+
+
