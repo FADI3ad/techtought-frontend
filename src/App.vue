64fa9ac32396b16c0c6bbc51d25b36f6f3a79622
mariam <mkopibhh03@gmail.com>
2026-02-01 23:25:46 +0200

    WPI: current progress

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,130 @@
 </script>
 
 <template>
+<div>
+    <!-- Header -->
+    <header class="header-top">
+      <div class="logo">LOGO</div>
+<div class="search-box">
+        <input
+          type="text"
+          placeholder="Search for files, plugins, and creators"
+        />
+        <span class="search-icon">🔍</span>
+      </div>
+
+<div class="top-text">
+  <span>About</span>
+  <span>Tech Taught</span>
+  <span>Contact Us</span>
+  <span>My Learning</span>
+</div>
+    </header>
+
+ <div class="categories">
+  <div class="category">Computer Fundamentals</div>
+  <div class="category">Web Development</div>
+  <div class="category">Mobile Development</div>
+  <div class="category">Desktop Development</div>
+</div>
+
+
+    <!-- Welcome -->
+    <div class="welcome-divider"></div>
+<section class="welcome">
+  <h2>Welcome back, <span>Sandra</span></h2>
+</section>
+  </div>
+
+
+
+<section class="trending-courses">
+  <h2>Trending Courses</h2>
+  <p>We know the best things for you, top picks for you</p>
+</section>
+
+
+ 
+<section class="cards-section">
+  <div class="cards-row">
+    <div class="card">
+      <img src="" alt="Course 1" />
+      <p>Beginner guide to becoming a professional Frontend Developer</p>
+    </div>
+
+    <div class="card">
+      <img src=" " alt="Course 2" />
+      <p>Beginner guide to becoming a professional Frontend Developer</p>
+    </div>
+
+    <div class="card">
+      <img src=" " alt="Course 3" />
+      <p>Beginner guide to becoming a professional Frontend Developer</p>
+    </div>
+
+    <div class="card">
+      <img src=" " alt="Course 4" />
+      <p>Beginner guide to becoming a professional Frontend Developer</p>
+    </div>
+
+    <div class="card">
+      <img src=" " alt="Course 5" />
+      <p>Beginner guide to becoming a professional Frontend Developer</p>
+    </div>
+  </div>
+</section>
+
 
 </template>
 
 <style scoped>
 
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
