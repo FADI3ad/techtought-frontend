9b148351eda292126baef8edd40f304a43842698
mariam <mkopibhh03@gmail.com>
2026-02-14 22:42:38 +0200

    web-development

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,246 @@
 </script>
 
 <template>
+<header class="header">
+
+  <div class="header-top">
+
+    <div class="logo">LOGO</div>
+
+    <div class="search-box">
+      <input type="text" placeholder="Search for files, plugins, and creators"/>
+      <span class="search-icon">🔍</span>
+    </div>
+
+    <div class="header-text">
+      <span>About Tech Taught</span>
+      <span>Contact Us</span>
+      <span>My learning</span>
+    </div>
+
+    <div class="header-icons">
+      <span class="heart">♡</span>
+      <div class="avatar">
+      <img src="./avatar.jpeg" alt="User Avatar" />
+      </div>
+    </div>
+  </div>
+
+  <div class="header-divider"></div>
+
+  <div class="categories">
+    <span class="active">Web Development</span>
+    <span>Front End</span>
+    <span>Back End</span>
+    <span>Databases</span>
+  </div>
+
+<div class="categories-divider"></div>
+</header>
+
+
+<section class="hero-section">
+  <div class="hero-text">
+    <h1>Web Development</h1>
+    <p>
+      TOTC is one powerful online software suite that combines all the tools needed
+    </p>
+  </div>
+  <div class="image">
+    <img src="./mobile.jpeg" alt="Mobile" />
+  </div>
+</section>
+
+
+<section class="courses-intro">
+  <h2>Courses to get you started</h2>
+  <p>We know the best things for you. Top picks for you.</p>
+
+<div class="courses-cards">
+<div class="course-card">
+  <img class="course-image" src="" alt=""/>
+
+  <h3>Beginner’s Guide To Becoming A Professional Frontend Developer</h3>
+
+  <div class="course-meta">
+    <span>⏱ 10 weeks</span>
+    <span>📘24 lessons</span>
+  </div>
+
+  <div class="course-author">
+    <span class="user-name">Robert Martinez</span>
+    <span class="rating">⭐⭐⭐⭐⭐4.8</span>
+  </div>
+
+  <button class="course-btn">▶ Complete Course</button>
+</div>
+
+<div class="course-card">
+  <img class="course-image" src="" alt=""/>
+
+  <h3>Beginner’s Guide To Becoming A Professional Frontend Developer</h3>
+
+  <div class="course-meta">
+    <span>⏱ 10 weeks</span>
+    <span>📘24 lessons</span>
+  </div>
+
+  <div class="course-author">
+     <span class="user-name">Robert Martinez</span>
+    <span class="rating">⭐⭐⭐⭐⭐4.8</span>
+  </div>
+
+  <button class="course-btn">▶ Complete Course</button>
+</div>
+
+<div class="course-card">
+  <img class="course-image" src="" alt=""/>
+
+  <h3>Beginner’s Guide To Becoming A Professional Frontend Developer</h3>
+
+  <div class="course-meta">
+    <span>⏱ 10 weeks</span>
+    <span>📘24 lessons</span>
+  </div>
+
+  <div class="course-author">
+     <span class="user-name">Robert Martinez</span>
+    <span class="rating">⭐⭐⭐⭐⭐4.8</span>
+  </div>
+
+  <button class="course-btn">▶ Complete Course</button>
+</div>
+
+<div class="course-card">
+  <img class="course-image" src="" alt=""/>
+
+  <h3>Beginner’s Guide To Becoming A Professional Frontend Developer</h3>
+
+  <div class="course-meta">
+    <span>⏱ 10 weeks</span>
+    <span>📘24 lessons</span>
+  </div>
+
+  <div class="course-author">
+    <span class="user-name">Robert Martinez</span>
+    <span class="rating">⭐⭐⭐⭐⭐4.8</span>
+  </div>
+
+  <button class="course-btn">▶ Complete Course</button>
+</div>
+
+<div class="course-card">
+  <img class="course-image" src="" alt=""/>
+
+  <h3>Beginner’s Guide To Becoming A Professional Frontend Developer</h3>
+
+  <div class="course-meta">
+    <span>⏱ 10 weeks</span>
+    <span>📘24 lessons</span>
+  </div>
+
+  <div class="course-author">
+     <span class="user-name">Robert Martinez</span>
+    <span class="rating"> ⭐⭐⭐⭐⭐4.8</span>
+  </div>
+
+  <button class="course-btn">▶ Complete Course</button>
+</div>
+
+</div>
+</section>
+
+
+
+<section class="courses-section">
+  <h2>All Web Development</h2>
+  <p class="subtitle">We know the best things for you. Top picks for you.</p>
+
+  <div class="course-row">
+    <img class="course-img" src="" alt="">
+
+    <div class="course-info">
+      <h3>Beginner’s Guide To Becoming A Professional Frontend Developer</h3>
+      <p class="course-desc">
+        Think Like A Senior Dev: Draft With AI Prompt & Then Refine Code | Next.js 
+        <br>
+        15, React 19 ,Typescript ,Prisma ,Zod ,Tailwind4
+      </p>
+
+      <div class="rating">4.8 ⭐⭐⭐⭐⭐ </div>
+      <div class="avatar">
+        <img class="user-avatar" src="./avatar.jpeg" alt="user-avatar">
+       <span class="user-name">Robert Martinez</span>
+      </div>
+    </div>
+
+    <div class="course-price">E£519.99</div>
+  </div>
+
+  <div class="course-row">
+    <img class="course-img" src="" alt="">
+
+    <div class="course-info">
+      <h3>Beginner’s Guide To Becoming A Professional Frontend Developer</h3>
+      <p class="course-desc">
+        Think Like A Senior Dev: Draft With AI Prompt & Then Refine Code | Next.js 
+        <br>
+        15, React 19 ,Typescript ,Prisma ,Zod ,Tailwind4
+      </p>
+
+      <div class="rating">4.8 ⭐⭐⭐⭐⭐ </div>
+      <div class="avatar">
+        <img class="user-avatar" src="./avatar.jpeg" alt="user-avatar">
+        <span class="user-name">Robert Martinez</span>
+      </div>
+    </div>
+
+    <div class="course-price">Free</div>
+  </div>
+
+  <div class="course-row">
+    <img class="course-img" src="" alt="">
+
+    <div class="course-info">
+      <h3>Beginner’s Guide To Becoming A Professional Frontend Developer</h3>
+      <p class="course-desc">
+        Think Like A Senior Dev: Draft With AI Prompt & Then Refine Code | Next.js
+        <br>
+         15, React 19 ,Typescript ,Prisma ,Zod ,Tailwind4
+      </p>
+
+      <div class="rating">4.8 ⭐⭐⭐⭐⭐ </div>
+      <div class="avatar">
+        <img class="user-avatar" src="./avatar.jpeg" alt="user-avatar">
+        <span class="user-name">Robert Martinez</span>
+      </div>
+    </div>
+
+    <div class="course-price">E£519.99</div>
+  </div>
+
+  <div class="course-row">
+    <img class="course-img" src="" alt="">
+
+    <div class="course-info">
+      <h3>Beginner’s Guide To Becoming A Professional Frontend Developer</h3>
+      <p class="course-desc">
+        Think Like A Senior Dev: Draft With AI Prompt & Then Refine Code | Next.js 
+        <br>
+        15, React 19 ,Typescript ,Prisma ,Zod ,Tailwind4
+      </p>
+
+      <div class="rating">4.8 ⭐⭐⭐⭐⭐ </div>
+      <div class="avatar">
+        <img class="user-avatar" src="./avatar.jpeg" alt="user-avatar">
+        <span class="user-name">Robert Martinez</span>
+      </div>
+    </div>
+
+    <div class="course-price">Free</div>
+  </div>
+
+</section>
 
 </template>
 
